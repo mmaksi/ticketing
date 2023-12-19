609691e349830ed6091c51dd42bc7e80e7298905
mmaksi <mmaksi.dev@gmail.com>
2023-12-19 17:38:30 +0100

    changed the behaviour of error handling in Express to handle async functions using the throw keyword instead of calling the new keyword

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
@@ -14,7 +15,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new ErrorNotFound();
 });
 
